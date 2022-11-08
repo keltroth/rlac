c4f0cdf02280c89e715808c1dc416d1a1d6f43bb
Django Janny <django.janny@gmail.com>
2022-11-09 00:30:45 +0100

    Test is better with more APIs and a allSettled promises call

--- test.js
+++ test.js
@@ -1,48 +1,65 @@
 import { Rlac } from './index.js';
 
 const api = {
-        tester(value) {
-                console.log(`Yeah ${value}`);
-        }
+    tester(value) {
+        console.log(`Yeah ${value}`);
+    },
 };
 
 const test = new Rlac(api, 'yeah', 3000, 10);
 const test2 = new Rlac(api, 'yeah', 3000, 10);
+const test3 = new Rlac(api, 'yeah2', 3000, 10);
 
 
 let idx = 0;
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test2.tester(idx++);
-await test.tester(idx++);
-await test2.tester(idx++);
-await test2.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test2.tester(idx++);
-await test2.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test2.tester(idx++);
-await test2.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
-await test.tester(idx++);
 
-process.exit();
+Promise.allSettled([
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test2.tester(idx++),
+        test.tester(idx++),
+        test2.tester(idx++),
+        test2.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test2.tester(idx++),
+        test2.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test2.tester(idx++),
+        test2.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test3.tester(idx++),
+        test3.tester(idx++),
+        test3.tester(idx++),
+        test3.tester(idx++),
+        test3.tester(idx++),
+        test3.tester(idx++),
+        test3.tester(idx++),
+        test3.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+        test.tester(idx++),
+    ],
+).then((data) => {
+    console.log(data);
+    process.exit();
+});
+
+
